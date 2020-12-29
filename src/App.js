adb5012f21ae5acd728294cf5b516a14cba246f5
sofia-gi <68931730+sofia-gi@users.noreply.github.com>
2020-12-29 17:06:03 +0900

    Immutable
    Immutable

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { List } from 'immutable';
 import React, { Component } from 'react';
 import Form from './componentes/Form';
 import WebItemColor from './componentes/WebItemColor';
@@ -21,8 +22,34 @@ class App extends Component{
     color: '#343a40'
   }
 
-  //function Form
+  //Immutable : 기존의 객체는 건들이지 않고 새 객체를 생성하여 불면함을 유지하며 값을 업데이트하기 위해 사용
 
+/*  state = {
+    data: Map({
+      input: '',
+      todos: List([
+        Map({
+          id: 0,
+          text: 'react',
+          checked: false
+        }),
+        Map({
+          id: 1,
+          text: 'Immutable',
+          checked: true
+        }),
+        Map({
+          id: 2,
+          text: 'setting',
+          checked: false
+        })
+      ]),      
+      color: '#343a40'
+    })
+  }
+*/
+
+  //function Form
   handleChange = (e) => {
     this.setState({
       input: e.target.value
@@ -37,11 +64,25 @@ class App extends Component{
         id: this.id++,
         text: input,
         checked: false,
-        color
+        color: color
       })
     });
   }
 
+  /*
+  Immutable 
+  handleCreate = () => {
+    const { data } = this.state;
+    this.setState({
+      data: data.set('input', '').update('todos', todos => todos.push(Map({
+        id: this.id++,
+        text: data.get('input'),
+        checked: false,
+        color: data.get('color')
+      })))
+      })
+  }  
+*/
   handleKeyPress = (e) => {
     if(e.key == 'Enter'){
       this.handleCreate();
@@ -112,7 +153,7 @@ class App extends Component{
           onKeyPress={handleKeyPress}
           onChange={handleChange}
           onCreate={handleCreate}
-          color={color}
+          style={color}
         />
       )}
         palette={(
